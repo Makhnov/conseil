2c3a561a75a27d7bcd2e9ea535ad5e59e152b52d
Makhnov <makh@tutanota.com>
2023-07-08 09:30:54 +0200

    s

--- mvc/vue/header.php
+++ mvc/vue/header.php
@@ -11,7 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <?php assets('css', 'header'); ?>
-        <link rel="icon" type="image/x-icon" href="<?php echo get_url('/img/favdnd.ico') ?>">
+        <link rel="icon" type="image/x-icon" href="https://makh.fr/divers/img/favdnd.ico">
         <?php echo '<'.'?xml version="1.0" encoding="utf-8"?'.'>';?>
     </head>
         <body class="<?php echo $class; ?>" <?php //if ($class === 'accueil') { [FINPHPHERE] onload="init()"<?php } ?>>
